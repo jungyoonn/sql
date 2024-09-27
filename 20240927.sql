162f2caa4e8d8ee1c885896c3a723bdb5f42a6a3
jungyoonn <sophia7625@naver.com>
2024-09-27 10:17:16 +0900

    데이터 타입의 변환

---
+++ 20240927.sql
@@ -0,0 +1,26 @@
+DROP TABLE STUD_HEAVY;
+DROP TABLE STUD_101 ;
+DROP TABLE EX_TYPE ;
+
+-- 전인하 학생의 생년월일 중 연, 월을 출력(YY-MM)
+SELECT BIRTHDATE , TO_CHAR(BIRTHDATE, 'YY-MM') 
+FROM STUDENT
+WHERE NAME = '전인하';
+
+-- 102번 학과 학생의 이름, 학년, 생년월일을 출력
+SELECT NAME, GRADE, BIRTHDATE , TO_CHAR(BIRTHDATE, 'DAY MONTH DD YYYY') 생년월일 
+FROM STUDENT
+WHERE DEPTNO = 102;
+
+-- 보직수당을 받는 교수들을 대상으로 수당과 급여를 더한 값에 12를 곱하여 ANUAL
+-- 라는 이름으로 연봉 계산을 하고 세 자리마다 쉼표로 표시
+SELECT NAME, SAL, COMM, TO_CHAR((SAL+COMM)*12, '9,999') ANUAL
+FROM PROFESSOR
+WHERE COMM IS NOT NULL;
+
+SELECT 
+	TO_NUMBER('1234'),
+--	TO_NUMBER('ABCD'), 
+	TO_NUMBER('1,234', '9,999') + 1111 -- 그냥은 인식을 못하므로 포맷을 해 주어야 함
+FROM DUAL;
+
